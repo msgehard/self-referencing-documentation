a464cc51a785924fa3d586d5c653544412df2a60
Mike Gehard <mike.gehard@gmail.com>
2014-06-02 09:31:42 -0600

    Extract some methods
    This makes it a bit easier to read and figure out what
    is going on

--- lib/dictionary.rb
+++ lib/dictionary.rb
@@ -8,29 +8,25 @@ class Dictionary
   def definition_for(word)
     definition = @contents[word]["definition"]
 
-    related_words = definition.scan(/{(.*?)}/).flatten
-
-    related_urls = related_words.map do |related_word|
-
-      if found_related_word?(related_word)
-        url_for(related_word)
-
-      else
-        url_for_singular(related_word)
-      end
-    end
-
     {
-        definition: definition.gsub(/{/, '').gsub(/}/, ''),
-        see_also: related_urls
+        definition: definition_for_display(definition),
+        see_also: related_urls(definition)
     }
   end
 
   private
+
+  def definition_for_display(definition)
+    definition.gsub(/{/, '').gsub(/}/, '')
+  end
+
+  def related_words(definition)
+    definition.scan(/{(.*?)}/).flatten
+  end
+
   def url_for_singular(related_word)
     @contents[related_word[0..-2]]["url"]
   end
-
 
   def url_for(related_word)
     @contents[related_word]["url"]
@@ -38,6 +34,17 @@ class Dictionary
 
   def found_related_word?(related_word)
     !@contents[related_word].nil?
+  end
+
+  def related_urls(definition)
+    related_words(definition).map do |related_word|
+
+      if found_related_word?(related_word)
+        url_for(related_word)
+      else
+        url_for_singular(related_word)
+      end
+    end
   end
 
 end
